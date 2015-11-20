c527b0575e7a2ff728ec79e01be3d39d220a45eb
guohy <guohy@zouzhe.com>
2015-11-20 10:48:53 +0800

    a lot animation

--- src/com/example/animdemo/MainActivity.java
+++ src/com/example/animdemo/MainActivity.java
@@ -1,29 +1,53 @@
 package com.example.animdemo;
 
+import com.sdf.jazzylistviewdemo.widget.jazzylistview.JazzyHelper;
+import com.sdf.jazzylistviewdemo.widget.jazzylistview.JazzyListView;
+
+import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
-public class MainActivity extends BaseActivity {
-	private EditText et_search;
-
-	private IconCenterEditText icet_search;
+public class MainActivity extends Activity {
+	private JazzyListView lv;
 
 	@Override
-	public void iniView() {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		et_search = (EditText) findViewById(R.id.et_search);
-		icet_search = (IconCenterEditText) findViewById(R.id.icet_search);
-
-		// 实现TextWatcher监听即可
-		icet_search
-				.setOnSearchClickListener(new IconCenterEditText.OnSearchClickListener() {
-					@Override
-					public void onSearchClick(View view) {
-						Toast.makeText(MainActivity.this, "i'm going to seach",
-								Toast.LENGTH_SHORT).show();
-					}
-				});
+		lv = (JazzyListView) findViewById(R.id.lv);
+		lv.setAdapter(new MyAdapter());
+		lv.setTransitionEffect(JazzyHelper.FAN);
 	}
+
+	class MyAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 20;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = View.inflate(getApplicationContext(),
+					R.layout.list_item, null);
+			return view;
+		}
+	}
+
 }
